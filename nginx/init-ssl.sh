c68af0361351e7361263bd4fa636fc1e22845b58
rozoomcool <rozoom.um@gmail.com>
2024-04-01 02:16:08 +0300

    fix nginx conf

--- nginx/init-ssl.sh
+++ nginx/init-ssl.sh
@@ -3,6 +3,6 @@ service nginx start
 apt-get update && apt-get install python3 python3-venv python3-pip -y
 python3 -m venv /venv
 /venv/bin/pip install certbot certbot-nginx
-/venv/bin/certbot certonly --standalone --non-interactive --agree-tos --email rosul.um@gmail.com -d ldent.online
+#/venv/bin/certbot certonly --standalone --agree-tos --email rosul.um@gmail.com -d ldent.online
 
-nginx -s reload
+#nginx -s reload
