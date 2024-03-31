b3a56076b3b093719781adb69743195c1a2534da
rozoomcool <rozoom.um@gmail.com>
2024-03-31 23:02:12 +0300

    fix certbot options

--- init-ssl.sh
+++ init-ssl.sh
@@ -1,3 +1,8 @@
+apt update
+apt install snapd
+snap install core; sudo snap refresh core
+snap install --classic certbot
+ln -s /snap/bin/certbot /usr/bin/certbot
 certbot certonly --standalone --non-interactive --agree-tos --email rosul.um@gmail.com -d ldent.online
 
 nginx -g 'daemon off;'
