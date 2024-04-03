03cb9d77e23e90b96f73a0b6f0b7e71dacd6e243
rozoomcool <rozoom.um@gmail.com>
2024-04-03 04:40:23 +0300

    setup csrf

--- dentalclinic/dentalclinic/settings.py
+++ dentalclinic/dentalclinic/settings.py
@@ -135,6 +135,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://ldent.online'
+]
+
 STATIC_URL = '/static/'
 STATIC_ROOT = f"{BASE_DIR}/static/"
 
